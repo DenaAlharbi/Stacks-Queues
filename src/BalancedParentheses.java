f6370cc707f7fc31aa61d8a6eeb6c02e9fc29695
DenaAlharbi <denauni2022@gmail.com>
2024-09-23 13:26:00 +0300

    test

--- src/BalancedParentheses.java
+++ src/BalancedParentheses.java
@@ -4,6 +4,7 @@ public class BalancedParentheses {
     public static void main(String[] args) {
         LabStack<Character> s = new LabStack<>();
         Scanner k = new Scanner(System.in);
+        //
         System.out.print("Enter a mathematical expression> ");
         String input = k.nextLine();
 
