0c875fcbe357c202f3023eb9b24296ae90b88b77
DenaAlharbi <denauni2022@gmail.com>
2024-09-23 14:20:21 +0300

    test

---
+++ src/PostfixEvaluater.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class PostfixEvaluater {
+    public static void main(String[] args) {
+        LabStack<Character> s = new LabStack<>();
+        Scanner k = new Scanner(System.in);
+        //
+        System.out.print("Enter a mathematical expression> ");
+        String input = k.nextLine();
+
+        for (char charInArray : input.toCharArray()) {
+
+        }
+    }
+}
